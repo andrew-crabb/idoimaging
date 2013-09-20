edf236ff7faa83dedf0c304104a339f93bdf9335
andrewcrabb <ahc@me.com>
2013-09-20 17:17:47 -0400

    debug info in get_user_details

---
+++ cgi-bin/test_user.pl
@@ -0,0 +1,28 @@
+#! /usr/local/bin/perl -w
+
+use CGI;
+use CGI::Carp;
+use DBI;
+use FindBin qw($Bin);
+use lib $Bin;
+use Utility;
+use radutils;
+use Userbase;
+use bigint;
+use strict;
+no strict 'refs';
+
+my $cgi = new CGI;
+
+# Redirect unless this has come from me, not a search engine.
+my $referer = $cgi->referer();
+
+print $cgi->header();
+
+# If adding program, ensure logged-in user is admin.
+my $det = get_user_details();
+unless ($det and $det->{$Userbase::UB_IS_ADMIN}) {
+  print "<tt>Edit a program?  I don't think so.</tt><br>\n";
+  get_user_details(1);
+  exit;
+}
