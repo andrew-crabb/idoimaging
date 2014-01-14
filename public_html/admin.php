bb7ff25dc1aa98351d491c909d62c42853dba23d
Andrew Crabb <ahc@me.com>
2014-01-14 11:37:53 -0500

    small changes

--- public_html/admin.php
+++ public_html/admin.php
@@ -1,6 +1,6 @@
 <?php 
 $groups_allowed = "admin"; 
-$users_allowed = 'ahc';
+# $users_allowed = 'ahc';
 require($_SERVER['DOCUMENT_ROOT'] . "/login/ublock.php"); 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
